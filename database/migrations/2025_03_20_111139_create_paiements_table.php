c893f2280a09274d672f9e8515e0fba7751cfb42
charafeddine-Web <charafeddinetbibzat@gmail.com>
2025-03-26 15:02:20 +0000

    add migrationn de paiments and tickets

--- database/migrations/2025_03_20_111139_create_paiements_table.php
+++ database/migrations/2025_03_20_111139_create_paiements_table.php
@@ -1,31 +1,22 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+return new class extends Migration {
+    public function up()
     {
         Schema::create('paiements', function (Blueprint $table) {
             $table->id();
-            $table->float('montant');
-            $table->enum('status', ['payé', 'en attente', 'annulé'])->default('en attente');
             $table->foreignId('reservation_id')->constrained('reservations')->onDelete('cascade');
             $table->foreignId('siege_id')->constrained('sieges')->onDelete('cascade');
-            $table->foreignId('ticket_id')->constrained('tickets')->onDelete('cascade');
+            $table->decimal('montant', 10, 2);
+            $table->enum('status', ['en attente', 'payé', 'annulé'])->default('en attente');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('paiements');
     }
